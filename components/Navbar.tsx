d5e5534e0f802d574ccd4cac183061351b447919
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-26 14:55:10 +0600

    replace AcmeLogo with Image component for logo display; add ScentRGB.png

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,19 +11,8 @@ import {
   Link,
   Button,
 } from "@nextui-org/react";
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import logo from '@/public/ScentRGB.png'
+import Image from "next/image";
 
 export default function NavbarTop() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -49,15 +38,13 @@ export default function NavbarTop() {
 
       <NavbarContent className="sm:hidden pr-3" justify="center">
         <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
+          <Image src={logo} alt="ACME" width={100} height={100} />
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
+          <Image src={logo} alt="ACME" width={100} height={100} />
         </NavbarBrand>
         <NavbarItem>
           <Link color="foreground" href="#">
@@ -77,12 +64,9 @@ export default function NavbarTop() {
       </NavbarContent>
 
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
+          <Button as={Link} className="px-10 font-bold" href="#" variant="solid" radius="full" >
+            Login
           </Button>
         </NavbarItem>
       </NavbarContent>
